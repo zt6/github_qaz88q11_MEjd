aa30f65b25af8b82bba3dfbce578c47178375c1f
qaz88q11 <78058188+qaz88q11@users.noreply.github.com>
2021-06-16 13:44:55 +0800

    Add files via upload

---
+++ jdJxncTokens.js
@@ -0,0 +1,30 @@
+/*
+京喜农场 Tokens
+此文件为Node.js专用。其他用户请忽略
+支持京东N个账号
+ */
+// 每个账号 token 是一个 json，示例如下
+// {"farm_jstoken":"749a90f871adsfads8ffda7bf3b1576760","timestamp":"1610165423873","phoneid":"42c7e3dadfadsfdsaac-18f0e4f4a0cf"}
+let JxncTokens = [
+  '{"farm_jstoken":"f06636c426fda4c48e025edc12271f4d","timestamp":"1623663557000","phoneid":"39593d9fe6240ebd"}',//账号一的京喜农场token
+  '{"farm_jstoken":"324ac8a8353178cab94f2b91551768c0","timestamp":"1623710533422","phoneid":"9418e8fee9c0ce96bc32b8a07c0c31482ac7a963"}',//账号二的京喜农场token
+]
+// 判断github action里面是否有京喜农场 token 
+if (process.env.JXNCTOKENS) {
+  if (process.env.JXNCTOKENS.indexOf('&') > -1) {
+    console.log(`您的京喜农场 token 选择的是用&隔开\n`)
+    JxncTokens = process.env.JXNCTOKENS.split('&');
+  } else if (process.env.JXNCTOKENS.indexOf('\n') > -1) {
+    console.log(`您的京喜农场 token 选择的是用换行隔开\n`)
+    JxncTokens = process.env.JXNCTOKENS.split('\n');
+  } else {
+    JxncTokens = process.env.JXNCTOKENS.split();
+  }
+} else if (process.env.JD_COOKIE) {
+  console.log(`由于您secret里面未提供 tokens，当种植 APP 种子时，将不能正常进行任务，请提供 token 或 种植非 APP 种子！`)
+}
+JxncTokens = [...new Set(JxncTokens.filter(item => !!item))]
+for (let i = 0; i < JxncTokens.length; i++) {
+  const index = (i + 1 === 1) ? '' : (i + 1);
+  exports['JXNCTOKEN' + index] = JxncTokens[i];
+}
